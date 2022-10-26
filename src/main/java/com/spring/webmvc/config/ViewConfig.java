2e30eac32b087cb8449bf250d49f70cd88976982
lemoncoat <joy961027@naver.com>
2022-10-26 16:25:01 +0900

    2022.10.26 pm 04:23 -spring boot 설정 파일 생성 방법 및 viewresolver 설정 방법 및 컨트롤러 연동

---
+++ src/main/java/com/spring/webmvc/config/ViewConfig.java
@@ -0,0 +1,18 @@
+package com.spring.webmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//@Configuration
+public class ViewConfig {
+//    @Bean
+//    public InternalResourceViewResolver resolver(){
+//        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+//        internalResourceViewResolver.setPrefix("WEB-INF/views/");
+//        internalResourceViewResolver.setSuffix(".jsp");
+//
+//        return internalResourceViewResolver;
+//
+//    }
+}
